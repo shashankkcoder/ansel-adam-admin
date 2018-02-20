81ac613596650cda0064cfecf83a5f8baa4156ce
IDX Chi <choang@optimags.com>
2018-02-20 11:35:07 -0800

    Add deploy instructions
    Modify default route

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -7,7 +7,8 @@ const routes: Routes = [
         path: '',
         component: LayoutComponent,
         children: [
-            { path: '', redirectTo: 'dashboard' },
+            // { path: '', redirectTo: 'dashboard' },
+            { path: '', redirectTo: 'all-photos' },
             { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
             { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
             { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
