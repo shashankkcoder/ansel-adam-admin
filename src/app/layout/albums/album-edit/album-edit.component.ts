4e175aa36506ce7588386c54db0b46cb305812eb
IDX Chi <choang@optimags.com>
2018-03-08 16:11:31 -0800

    Add album edit page

--- src/app/layout/albums/album-edit/album-edit.component.ts
+++ src/app/layout/albums/album-edit/album-edit.component.ts
@@ -1,4 +1,10 @@
+import { FormModule } from './../../form/form.module';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlbumService } from './../../../service/album.service';
+import { Album } from './../../../model/album';
+import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-album-edit',
@@ -7,9 +13,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlbumEditComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  album$: Observable<Album>;
+  albumName: string;
+  albumDescription: string;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private albumService: AlbumService,
+              private location: Location) { }
 
   ngOnInit() {
+    // allows navigate to the same page
+    this.route.paramMap.subscribe(params => {
+      this.id = +params.get('id');
+    });
+
+    this.album$ = this.albumService.getAlbumWithId(this.id);
   }
 
+  onSubmit(albumId, albumName, albumDescription) {
+
+    alert("editing album id: " + albumId + " " + albumName + " " + albumDescription);
+  }
+
+  onCancel() {
+    this.location.back();
+    // this.router.navigateByUrl('/albums');
+  }
 }
--- src/app/layout/albums/albums.module.ts
+++ src/app/layout/albums/albums.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -9,7 +10,8 @@ import { AlbumEditComponent } from './album-edit/album-edit.component';
 @NgModule({
   imports: [
     CommonModule,
-    AlbumsRoutingModule
+    AlbumsRoutingModule,
+    FormsModule
   ],
   declarations: [AlbumsComponent, AlbumDetailsComponent, AlbumEditComponent]
 })
