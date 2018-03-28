1cb366cd710361c1b68190b28ceee3082c12e404
IDX Chi <choang@optimags.com>
2018-03-28 09:14:19 -0700

    Add hidden field to album model

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -21,7 +21,6 @@ export class LoginComponent implements OnInit {
 
 	onLoggedin() {
 		this.auth.authenticate(this.email, this.password).subscribe(response => {
-			console.log(response);
 			if (response.userAccessToken && response.isAdmin === true) {
 				localStorage.setItem('userAccessToken', response.userAccessToken);
 				localStorage.setItem('isLoggedin', 'true');
