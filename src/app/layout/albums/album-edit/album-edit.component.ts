a2e923a340749085f918e8fffa9eafd4eae5888c
Mandeep <mspanesar145@gmail.com>
2018-05-17 18:07:46 +0530

    Added chanegs to show gallery instead of local computer

--- src/app/layout/albums/album-edit/album-edit.component.ts
+++ src/app/layout/albums/album-edit/album-edit.component.ts
@@ -1,4 +1,5 @@
 // import { FormModule } from './../../form/form.module';
+import { MyImage } from '../../../model/MyImage';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AlbumService } from './../../../service/album.service';
 import { Album } from './../../../model/album';
@@ -15,10 +16,15 @@ export class AlbumEditComponent implements OnInit {
 
   id: number;
   album$: Observable<Album>;
+  images$: Observable<MyImage[]>;
   // albumName: string;
   // albumDescription: string;
   imageFile: any;
   imageUrl: string;
+  display: any = '';
+  selectedCoverImage$: any;
+  selectedCoverImageUrl: string;
+
 
   constructor(private route: ActivatedRoute,
     private router: Router,
@@ -32,10 +38,11 @@ export class AlbumEditComponent implements OnInit {
     });
 
     this.album$ = this.albumService.getAlbumWithId(this.id);
-
     this.album$.subscribe(response => {
       this.imageUrl = response.defaultImageUrl;
     });
+    
+    this.images$ = this.albumService.getImagesWithAlbumId(this.id);
   }
 
   onFileChange(event) {
@@ -61,6 +68,10 @@ export class AlbumEditComponent implements OnInit {
       "description": albumDescription
     };
 
+    if (this.selectedCoverImage$) {
+      updateAlbum["defaultImageUrl"] =  this.selectedCoverImage$.url;
+    }
+
     this.albumService.updateAlbum(albumId, updateAlbum);
 
     alert("Album with id: " + albumId + " has been updated.");
@@ -69,4 +80,23 @@ export class AlbumEditComponent implements OnInit {
   onCancel() {
     this.location.back();
   }
+
+  openGalleryModal() {
+    this.display = 'block';
+  }
+  closeGalleryModal() {
+    this.display = 'none';
+  }
+
+  cancelUpdateCoverImage() {
+    this.selectedCoverImage$ = null;
+    this.closeGalleryModal();
+  }
+
+  selectedCoverImage (image) {
+    this.selectedCoverImage$ = image;
+    this.imageUrl = this.selectedCoverImage$.url;
+    this.closeGalleryModal();
+  }
+
 }
